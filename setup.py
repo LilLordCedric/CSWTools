2a9cb2d25a9252e8e3f0bd7beec064df58d21acb
Cedric Sascha Wagner <cedric.sascha.wagner@outlook.de>
2025-06-21 18:00:44 +0200

    Update package name in setup.py to 'cswtools'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="csw",
+    name="cswtools",
     version="0.1.0",
     author="Cedric Sascha wagner",
     author_email="cedric.sascha.wagner@outlook.de",
